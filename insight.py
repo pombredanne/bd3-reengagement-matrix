c9c73fda22a97126943343cddcedb6d69fa8c049
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-27 09:29:30 +0000

    implement the matrix of returning users

--- insight.py
+++ insight.py
@@ -1,14 +1,68 @@
+from bitdeli.widgets import Widget, Table
 from bitdeli.insight import insight
-from bitdeli.widgets import Text, Bar, Table
 from discodb.query import Q, Literal, Clause
 
-@insight
-def view(model, params):
-    params = {'events': ['$signup', 'Clip created']}
-    def steps(events):
-        for i in range(len(events)):
-            q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
-            yield events[i], len(model.query(q))
-    return [Bar(size=(12, 6),
-                data=list(steps(params['events'])))]
+class TokenInput(Widget):
+    pass
+
+BINS = ['0', '1', '2-3', '4-7', '8+']
+
+def month_keys(model):
+    keys = list(frozenset(key[:7] for key in model))
+    keys.sort(reverse=True)
+    return keys
     
+def months(params):
+    monthx = monthy = None
+    if 'monthx' in params and params['monthx']['value']:
+        monthx = params['monthx']['value'][0]
+    if 'monthy' in params and params['monthy']['value']:
+        monthy = params['monthy']['value'][0]
+    return monthx, monthy
+
+def rows(model, monthx, monthy):
+    def clauses(month):
+        q = [Literal('%s:%s' % (month, bin)) for bin in range(1, len(BINS))]
+        return [Clause(~x for x in q)] + [Clause([x]) for x in q]
+
+    def row(biny, y):
+        yield 'ybin', {'label': BINS[biny]}
+        for binx, x in enumerate(clauses(monthx)):
+            norm = float(max(1, len(model.query(Q([x | y])))))
+            perc = len(model.query(Q((y, x)))) / norm
+            yield BINS[binx], {'label': '%d%%' % (perc * 100),
+                               'background': perc}
+
+    return (dict(row(biny, y))
+            for biny, y in enumerate(clauses(monthy)))
+
+@insight
+def insight(model, params):
+    monthx, monthy = months(params)
+    
+    yield TokenInput(id='monthx',
+                     label='First Month',
+                     size=(6, 1),
+                     data=month_keys(model),
+                     value=[monthx] if monthx else [])
+    yield TokenInput(id='monthy',
+                     label='Second Month',
+                     size=(6, 1),
+                     data=month_keys(model),
+                     value=[monthy] if monthy else [])
+    
+    if monthx and monthy:
+        columns = [{'row_header': True,
+                    'label': monthy,
+                    'name': 'ybin'}] +\
+                  [{'label': bin, 'name': bin} for bin in BINS]
+        
+        rowss = list(rows(model, monthx, monthy))
+        print rowss
+        
+        yield Table(size=(12, 'auto'),
+                    label='% returning users over the frequency of use',
+                    fixed_width=True,
+                    columns_label=monthx,
+                    data={'columns': columns,
+                          'rows': rowss})
--- model.py
+++ model.py
@@ -1,7 +1,18 @@
+from collections import Counter
+from itertools import chain
+from datetime import datetime
+import math
+
 from bitdeli.model import model
 
 @model
-def build(profiles):
+def model(profiles):
+    
+    def months(events):
+        for hour, count in chain.from_iterable(events.itervalues()):
+            yield datetime.utcfromtimestamp(hour * 3600).strftime('%Y-%m')
+    
     for profile in profiles:
-        for event in profile['events']:
-            yield event, profile.uid
+        for month, count in Counter(months(profile['events'])).iteritems():
+            bin = min(int(math.log(count, 2)), 3) + 1
+            yield '%s:%s' % (month, bin), profile.uid
